d46a7eea7913560d2a122c726daa4daee22bd348
passt0r <dima3@ymail.com>
2017-09-07 01:03:17 +0300

    Finished second chapter app

--- Bow Ties/Bow Ties/AppDelegate.swift
+++ Bow Ties/Bow Ties/AppDelegate.swift
@@ -31,22 +31,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions
     launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Save test bow tie
-    let bowtie = NSEntityDescription.insertNewObject(
-      forEntityName: "Bowtie",
-      into: self.persistentContainer.viewContext) as! Bowtie
-    bowtie.name = "My bow tie"
-    bowtie.lastWorn = NSDate()
-    // Retrieve test bow tie
-    do {
-      let request = NSFetchRequest<Bowtie>(entityName: "Bowtie")
-      let ties =
-        try self.persistentContainer.viewContext.fetch(request)
-      let sample = ties.first
-      print("Name :\(sample?.name), Worn: \(sample?.lastWorn)")
-    } catch let error as NSError {
-      print("Fetching error: \(error), \(error.userInfo)")
+    guard let viewController = window?.rootViewController as? ViewController else {
+      return true
     }
+    viewController.managedContext = persistentContainer.viewContext
+    
     return true
   }
 

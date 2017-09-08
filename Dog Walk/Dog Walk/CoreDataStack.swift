e2b86275670a75e54bdb494950848230ac3f7a64
passt0r <dima3@ymail.com>
2017-09-08 21:28:59 +0300

    Start implementing Core Data stack

---
+++ Dog Walk/Dog Walk/CoreDataStack.swift
@@ -0,0 +1,33 @@
+//
+//  CoreDataStack.swift
+//  Dog Walk
+//
+//  Created by Dmytro Pasinchuk on 08.09.17.
+//  Copyright © 2017 Razeware. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+  private let modelName: String
+  
+  init(modelName: String) {
+    self.modelName = modelName
+  }
+  
+  private lazy var storeContainer: NSPersistentContainer = {
+    let container = NSPersistentContainer(name: self.modelName)
+    container.loadPersistentStores {
+      (storeDescription, error) in
+      if let error = error as NSError? {
+        print("Unresolved error \(error), \(error.userInfo)")
+      }
+    }
+    return container
+  }()
+  
+  lazy var managedContext: NSManagedObjectContext = {
+    return self.storeContainer.viewContext
+  }()
+}
